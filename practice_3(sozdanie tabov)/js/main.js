44d83d60497be78d3533e0642bb2de47a03fb690
unknown <aur0n17071@gmail.com>
2024-04-02 22:12:03 +0300

    end practice_3

--- practice_3(sozdanie tabov)/js/main.js
+++ practice_3(sozdanie tabov)/js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded' , () =>{
-  //Tabs
+  //Tabs ***
   let tabsContent = document.querySelectorAll('.tabcontent'),
       tabParent =document.querySelector('.tabheader__items'),
       tabs = document.querySelectorAll('.tabheader__item');
@@ -22,7 +22,9 @@ window.addEventListener('DOMContentLoaded' , () =>{
       }
       hideTabContent();
       showTabContent();
-
+      /*вешаем слушатель на родительский элемент и проверяем куда мы кликаем
+      если мы попали на нужный элемент то позкываем контект привязанный к элементу
+      */
       tabParent.addEventListener('click', (event) =>{
           const target = event.target;
           if (target && target.classList.contains('tabheader__item')) {
@@ -35,10 +37,13 @@ window.addEventListener('DOMContentLoaded' , () =>{
           }
       })
 
-    //Timer
+    //Timer ***
 
     const deadLine = '2024-12-30';
-
+      /*объявляем переменные и проверяем если дата прошла (Т отрицательная)
+      тогда ставим 0, иначе просчитываем время по каждой переменной через остаток от деления
+      выводим объект как результат выполнения функции
+      */
     function getTimeRemaining(endTime){
         let days, hours, minutes, seconds;
         const t = Date.parse(endTime) - Date.parse(new Date);
@@ -62,13 +67,14 @@ window.addEventListener('DOMContentLoaded' , () =>{
         seconds,
       }
     }
-
+    //добавляем 0 к однозначным числам
     function getZero(num){
         if (num >= 0 && num < 10){
             return `0${num}`;
         } else return num;
     }
 
+    //выбираем элементы из страницы и меняем их значение через интервал
     function setClock(selector, endTime){
        const timer = document.querySelector(selector),
              days = document.querySelector('#days'),
@@ -77,7 +83,7 @@ window.addEventListener('DOMContentLoaded' , () =>{
              seconds = document.querySelector('#seconds'),
              timeInterval = setInterval(updateClock, 1000);
 
-        updateClock();
+        updateClock(); //вызываем функцию сразу для простановки чисел
 
        function updateClock(){
           const t = getTimeRemaining(endTime);
@@ -93,4 +99,53 @@ window.addEventListener('DOMContentLoaded' , () =>{
     }
 
     setClock('.timer', deadLine);
+
+    //Modal ***
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-modal-close]');
+
+    function modalOpen (){
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden'; 
+        clearInterval(modalTimerId) //запрещает прокрутку страницы когда модалка открыта
+    }
+
+    function modalClose(){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalTrigger.forEach((item) => {
+      item.addEventListener('click', modalOpen)
+    })
+
+    modalCloseBtn.addEventListener('click', modalClose);
+
+    //закрытие модального окна при нажатии за пределами модалки
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modalClose();
+      }
+    })
+
+    //добавляем закрытие модалки при нажатии клавиши Esc, только когда модалка открыта
+    document.addEventListener('keydown' , (e) =>{
+      if (e.code === "Escape" && modal.classList.contains('show')) {      
+        modalClose();
+      }
+    })
+
+    //открытие модального окна по прошествию времени
+    const modalTimerId = setTimeout(modalOpen, 10000);
+
+    function showModalByScroll () {
+      if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+          modalOpen();
+          window.removeEventListener('scroll', showModalByScroll);
+      }
+  }
+    window.addEventListener('scroll', showModalByScroll)
 });
