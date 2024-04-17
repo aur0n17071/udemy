49f33dc03c8539a362900d0e4ec36de7eddc2512
unknown <aur0n17071@gmail.com>
2024-04-17 18:11:37 +0300

    add class for create cards in practice_3

--- practice_3(sozdanie tabov)/js/main.js
+++ practice_3(sozdanie tabov)/js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded' , () =>{
-  //Tabs ***
+  //Tabs ********************************************
   let tabsContent = document.querySelectorAll('.tabcontent'),
       tabParent =document.querySelector('.tabheader__items'),
       tabs = document.querySelectorAll('.tabheader__item');
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded' , () =>{
           }
       })
 
-    //Timer ***
+    //Timer ********************************************
 
     const deadLine = '2024-12-30';
       /*объявляем переменные и проверяем если дата прошла (Т отрицательная)
@@ -100,7 +100,7 @@ window.addEventListener('DOMContentLoaded' , () =>{
 
     setClock('.timer', deadLine);
 
-    //Modal ***
+    //Modal ********************************************
     const modalTrigger = document.querySelectorAll('[data-modal]'),
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-modal-close]');
@@ -139,7 +139,7 @@ window.addEventListener('DOMContentLoaded' , () =>{
     })
 
     //открытие модального окна по прошествию времени
-    const modalTimerId = setTimeout(modalOpen, 10000);
+    // const modalTimerId = setTimeout(modalOpen, 10000);
 
     function showModalByScroll () {
       if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
@@ -147,5 +147,70 @@ window.addEventListener('DOMContentLoaded' , () =>{
           window.removeEventListener('scroll', showModalByScroll);
       }
   }
-    window.addEventListener('scroll', showModalByScroll)
+    window.addEventListener('scroll', showModalByScroll);
+
+    
+    //Создание карточек при помощи классов*********************************************
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector){
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();  //вызов метода в конструкторе
+    }
+
+    changeToUAH(){
+      this.price = this.price * this.transfer;
+    }
+
+    render(){
+      let div = document.createElement('div');
+      div.innerHTML = `
+      <div class="menu__item">
+      <img src="${this.src}" alt="${this.alt}">
+      <h3 class="menu__item-subtitle">${this.title}"</h3>
+      <div class="menu__item-descr">${this.title}" - ${this.descr}</div>
+      <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+      </div>
+      `;
+      this.parent.append(div);
+    }
+  }
+
+  //создаем экземпляр объекта который мы используем и он удаляется без привязки к элементу
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
+  ).render();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню “Премиум”',
+    ' - мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    11,
+    '.menu .container'
+  ).render();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное',
+    ' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
+    10,
+    '.menu .container'
+  ).render();
 });
