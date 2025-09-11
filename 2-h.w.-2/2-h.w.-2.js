d79927bbc65bdf869df9bbd2e1f052d6d63d1d1e
Stas <113476325+aur0n17071@users.noreply.github.com>
2025-09-11 11:22:41 +0300

    1-h.w.-1,2
    Add 1-h.w.-1,2 js files

---
+++ 2-h.w.-2/2-h.w.-2.js
@@ -0,0 +1,11 @@
+const storeName = "name";
+
+const storeDescription = {
+    budget: 10000,
+    employees: ["Alex", "John", "Mike"],
+    products: {
+        kiwi: 100,
+        orange: 150
+    },
+    open: true
+};
