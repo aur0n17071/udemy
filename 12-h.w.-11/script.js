b06d996aec47bcd40b23de3f429db0a7c3ff393f
Stas <113476325+aur0n17071@users.noreply.github.com>
2025-09-23 23:47:10 +0300

    add 12 to 14 h.w.
    add 12 to 14 h.w.

---
+++ 12-h.w.-11/script.js
@@ -0,0 +1,61 @@
+'use strict'
+
+/*1) Напишите функцию showExperience, которая будет принимать в себя объект со всеми данными и возвращать строку с опытом.
+
+Пример:
+
+showExperience(personalPlanPeter) => '1 month'
+
+P.S. желательно использовать деструктуризацию, но не обязательно */
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%',
+            ruby: '30%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(obj){
+      return `Мне ${obj.age} и я владею языками: ${obj.skills.languages.map((item)=> item.toUpperCase()).join(' ')}`
+    }
+};
+
+function showExperience(plan) {
+  const {skills:{exp}} = plan;
+  return exp;
+}
+
+/*2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
+
+Пример:
+
+showProgrammingLangs(personalPlanPeter)  =>
+
+"Язык js изучен на 20% Язык php изучен на 10%"
+
+Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
+
+P.S. Для переноса строки используется \n в конце строки. */
+
+function showProgrammingLangs(plan) {
+  return Object.entries(plan.skills.programmingLangs).map(([lang,perc]) => `Язык ${lang} изучен на ${perc}`).join('\n')+'\n'
+}
+
+
+/*3) Создайте метод showAgeAndLangs внутри объекта personalPlanPeter. При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде.
+
+Пример:
+
+personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+=> 'Мне 29 и я владею языками: RU ENG'
+
+Заметьте, что возраст и языки подставляются автоматически из объекта, 
+а языки всегда в верхнем регистре (большими буквами). Если данные в объекте поменяются, 
+то и сообщение тоже изменится. */
+
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
