9216cbbe62cc0171c11012c8d4fe972e2899aae3
Stas <113476325+aur0n17071@users.noreply.github.com>
2026-05-25 23:36:38 +0300

    add tasks and update practice 1

--- Practice/7-1-Применяем-условия-и-циклы/script.js
+++ Practice/7-1-Применяем-условия-и-циклы/script.js
@@ -16,6 +16,16 @@
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+if (numberOfFilms > 0 && numberOfFilms < 10) {
+   console.log("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+   console.log("Вы классический зритель");
+} else if (numberOfFilms >= 30) {
+   console.log("Вы киноман");
+} else {
+   console.log("Произошла ошибка");
+}
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -27,7 +37,15 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++){
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = +prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[a] = b;
+
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+         personalMovieDB.movies[a] = b;
+      } else {
+         i--;
+      }
+         
 }
+
+
 
 console.log(personalMovieDB);
