bf51f8fcfabc0315e44557b81938807fc0d187e7
Stas <113476325+aur0n17071@users.noreply.github.com>
2026-06-22 12:44:54 +0300

    36-3-axios(CDN-type)
    Практика 3, часть 9 урок номер 36 в курсе

--- Practice/Project_3/js/script.js
+++ Practice/Project_3/js/script.js
@@ -148,43 +148,43 @@ window.addEventListener('DOMContentLoaded', () => {
     // CLASS CARD
 
     class MenuCard {
-        constructor(img, alt, title, text, price, parentSelector, ...classes) {
-            this.img = img;
-            this.alt = alt;
-            this.title = title;
-            this.text = text;
-            this.price = price;
-            this.classes = classes;
-            this.parentSelector = document.querySelector(parentSelector);
-            this.transfer = 27;
-            this.changeToUAH();
-        }
-
-        render(){
-            const card = document.createElement('div');
-            if (this.classes.length == 0) {
-                this.classes = 'menu__item';
-                card.classList.add(this.classes);
-            } else {
-                this.classes.forEach(className => card.classList.add(className))
+            constructor(img, alt, title, text, price, parentSelector, ...classes) {
+                this.img = img;
+                this.alt = alt;
+                this.title = title;
+                this.text = text;
+                this.price = price;
+                this.classes = classes;
+                this.parentSelector = document.querySelector(parentSelector);
+                this.transfer = 27;
+                this.changeToUAH();
             }
-            card.innerHTML = `
-                    <img src="${this.img}" alt="${this.alt}">
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.text}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>`;
-            this.parentSelector.append(card)
-        }
 
-        changeToUAH() {
-            this.price = this.price * this.transfer
-        }
+            render(){
+                const card = document.createElement('div');
+                if (this.classes.length == 0) {
+                    this.classes = 'menu__item';
+                    card.classList.add(this.classes);
+                } else {
+                    this.classes.forEach(className => card.classList.add(className))
+                }
+                card.innerHTML = `
+                        <img src="${this.img}" alt="${this.alt}">
+                        <h3 class="menu__item-subtitle">${this.title}</h3>
+                        <div class="menu__item-descr">${this.text}</div>
+                        <div class="menu__item-divider"></div>
+                        <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                        </div>`;
+                this.parentSelector.append(card)
+            }
 
-    }
+            changeToUAH() {
+                this.price = this.price * this.transfer
+            }
+
+        }
 
     //Reqiest for tabs create
     const getResource = async (url) => {
@@ -197,12 +197,19 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     }
 
-    getResource('http://localhost:3000/menu')
+    // getResource('http://localhost:3000/menu1')
+    // .then(data => {
+    //     data.forEach(({img, altimg, title, descr,price}) => {
+    //         new MenuCard(img, altimg, title, descr,price,'.menu .container').render();
+    //     });
+
+    axios.get('http://localhost:3000/menu')
     .then(data => {
-        data.forEach(({img, altimg, title, descr,price}) => {
+        data.data.forEach(({img, altimg, title, descr,price}) => {
             new MenuCard(img, altimg, title, descr,price,'.menu .container').render();
         });
-    })
+    });
+
 
     // SERVER SEND SCRIPT AND MODAL UPDATE
     const forms = document.querySelectorAll('form');
@@ -286,6 +293,6 @@ window.addEventListener('DOMContentLoaded', () => {
             modalClose();
         }, 3000)
     }
-    
 
-})
+
+});
