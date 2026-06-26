e8845fbe79ce8ce033c1a71b554c19c98c58ccd2
Stas <113476325+aur0n17071@users.noreply.github.com>
2026-06-26 16:15:41 +0300

    41-3-Калькулятор-на-сайте-часть-2

--- Practice/Project_3/js/script.js
+++ Practice/Project_3/js/script.js
@@ -400,9 +400,31 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // CALCULATOR
     const calcResult = document.querySelector('.calculating__result span');
-    let sex = 'female', 
-    height, weight, age, 
-    ratio = 1.375;
+
+    let sex = localStorage.getItem('sex') ?? 'female',
+        ratio = localStorage.getItem('ratio') ?? 1.375,
+        height, weight, age;
+
+    function calculatorInit(parentElement, activeClass){
+        const elements = document.querySelectorAll(`${parentElement} div`);
+
+        localStorage.setItem('sex', sex);
+        localStorage.setItem('ratio', ratio);
+
+        elements.forEach(el => {
+            el.classList.remove(activeClass);
+
+            if(el.getAttribute('id') === localStorage.getItem('sex')){
+                el.classList.add(activeClass);
+            }
+            if(el.getAttribute('data-ratio') === localStorage.getItem('ratio')){
+                el.classList.add(activeClass)
+            }
+        })
+    }
+
+    calculatorInit('#gender','calculating__choose-item_active')
+    calculatorInit('.calculating__choose_big','calculating__choose-item_active')
 
     function calculatorTotal(){
         if (!sex || !height || !weight || !age || !ratio){
@@ -427,8 +449,10 @@ window.addEventListener('DOMContentLoaded', () => {
             el.addEventListener('click', (e) => {
                 if (parentElement == '#gender') {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', sex);
                 } else {
-                    ratio = +e.target.getAttribute('data-ratio')
+                    ratio = +e.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', ratio);
                 }
 
                 elements.forEach(el => {
@@ -449,22 +473,31 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function calculatorDynamicInfo(){
         const element = document.querySelector('.calculating__choose_medium');
+
+
+
+        element.addEventListener('input', (e) => {
+
+            if (e.target.value.match(/\D/g)){
+                e.target.style.border = '1px solid red'
+            } else {
+                e.target.style.border = 'none'
+            }
+
+            switch(e.target.getAttribute('id')) {
+                case'height' :
+                    height = +e.target.value;
+                    break;
+                case'weight' :
+                    weight = +e.target.value;
+                    break;
+                case'age' :
+                    age = +e.target.value;
+                    break;
+            }
             
-            element.addEventListener('input', (e) => {
-                switch(e.target.getAttribute('id')) {
-                    case'height' :
-                        height = +e.target.value;
-                        break;
-                    case'weight' :
-                        weight = +e.target.value;
-                        break;
-                    case'age' :
-                        age = +e.target.value;
-                        break;
-                }
-                
-                calculatorTotal();
-            });
+            calculatorTotal();
+        });
     }
 
     calculatorDynamicInfo();
